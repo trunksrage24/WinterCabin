bc6f49ffcf9b421efc496d14f81a215b9ebed4a0
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2024-11-26 15:13:57 +0000

    Map work and final temporary ui work

--- WinterCabinGame/Assets/Scripts/Highlightable.cs
+++ WinterCabinGame/Assets/Scripts/Highlightable.cs
@@ -1,15 +1,27 @@
 using System;
+using System.Runtime.CompilerServices;
 using UnityEditor;
 using UnityEngine;
 
 public class Highlightable : Interactable
 {
     [field: SerializeField]
+    private Material highlight;
+
+    [field: SerializeField]
     public int id { get; private set; }
+
+    private bool isSelected = false;
 
     public override void Activate()
     {
         OnCLick?.Invoke(this);
+        isSelected = !isSelected;
+
+        if(isSelected)
+        {
+
+        }
         return;
     }
 
